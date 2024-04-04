651fa72d3a5a44732071015ff038a1afdb6a24b8
James Riehl <james.riehl@fetch.ai>
2024-04-04 10:42:59 +0100

    fix(#1): Added comments to config.py

--- python/src/uagents/config.py
+++ python/src/uagents/config.py
@@ -7,40 +7,63 @@ from uvicorn.logging import DefaultFormatter
 logging.basicConfig(level=logging.INFO)
 
 
+# Prefix used for agents.
 AGENT_PREFIX = "agent"
+# Prefix used for ledger.
 LEDGER_PREFIX = "fetch"
+# Prefix used for users.
 USER_PREFIX = "user"
+# Prefix used for testing net.
 TESTNET_PREFIX = "test-agent"
+# Prefix used for main network.
 MAINNET_PREFIX = "agent"
+# Length of the agent address.
 AGENT_ADDRESS_LENGTH = 65
 
+# Almanac contract for the main network.
 MAINNET_CONTRACT_ALMANAC = (
     "fetch1mezzhfj7qgveewzwzdk6lz5sae4dunpmmsjr9u7z0tpmdsae8zmquq3y0y"
 )
+# Almanac contract for the test network.
 TESTNET_CONTRACT_ALMANAC = (
     "fetch1tjagw8g8nn4cwuw00cf0m5tl4l6wfw9c0ue507fhx9e3yrsck8zs0l3q4w"
 )
+# Name service contract for the main network.
 MAINNET_CONTRACT_NAME_SERVICE = (
     "fetch1479lwv5vy8skute5cycuz727e55spkhxut0valrcm38x9caa2x8q99ef0q"
 )
+# Name service contract for the test network.
 TESTNET_CONTRACT_NAME_SERVICE = (
     "fetch1mxz8kn3l5ksaftx8a9pj9a6prpzk2uhxnqdkwuqvuh37tw80xu6qges77l"
 )
+# Fee to be paid for registration.
 REGISTRATION_FEE = 500000000000000000
+# The denomination of the fee to be paid for registration.
 REGISTRATION_DENOM = "atestfet"
+# Time in seconds between registration updates.
 REGISTRATION_UPDATE_INTERVAL_SECONDS = 3600
+# Time in seconds between registration retry attempts.
 REGISTRATION_RETRY_INTERVAL_SECONDS = 60
+# Average time interval between blocks.
 AVERAGE_BLOCK_INTERVAL = 6
 
+# The base URL for the agentverse.
 AGENTVERSE_URL = "https://agentverse.ai"
+# The API URL for the almanac.
 ALMANAC_API_URL = AGENTVERSE_URL + "/v1/almanac/"
+# Time interval between mailbox polling in seconds.
 MAILBOX_POLL_INTERVAL_SECONDS = 1.0
 
+# Time interval between wallet messaging polls in seconds.
 WALLET_MESSAGING_POLL_INTERVAL_SECONDS = 2.0
 
+# Suggested response time in seconds.
 RESPONSE_TIME_HINT_SECONDS = 5
+# Default timeout for envelope in seconds.
 DEFAULT_ENVELOPE_TIMEOUT_SECONDS = 30
+# Maximum number of end points by default.
 DEFAULT_MAX_ENDPOINTS = 10
+# Default search limit.
 DEFAULT_SEARCH_LIMIT = 100
 
 
@@ -49,6 +72,9 @@ def parse_endpoint_config(
 ) -> List[Dict[str, Any]]:
     """
     Parse the user-provided endpoint configuration.
+
+    Args:
+        endpoint: A string, list or dictionary representing the endpoint configuration.
 
     Returns:
         List[Dict[str, Any]]: The parsed endpoint configuration.
@@ -71,7 +97,10 @@ def parse_agentverse_config(
     config: Optional[Union[str, Dict[str, str]]] = None,
 ) -> Dict[str, str]:
     """
-    Parse the user-provided agentverse configutation.
+    Parse the user-provided agentverse configuration.
+
+    Args:
+        config: A string or dictionary representing the AgentVerse configuration.
 
     Returns:
         Dict[str, str]: The parsed agentverse configuration.
