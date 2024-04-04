bc0a258b2c1650a62dc702a3df984b000a5ec4be
James Riehl <james.riehl@fetch.ai>
2024-04-04 12:56:43 +0100

    fix(#2): Split single test case into two separate test cases in test_resolver.py

--- python/tests/test_resolver.py
+++ python/tests/test_resolver.py
@@ -1,6 +1,7 @@
 import unittest
 from uagents.resolver import RulesBasedResolver
 
+
 class TestRulesBasedResolver(unittest.TestCase):
     def setUp(self):
         self.resolver = RulesBasedResolver(
@@ -9,11 +10,13 @@ class TestRulesBasedResolver(unittest.TestCase):
             }
         )
 
-    def test_resolve(self):
-        destination, endpoints = await self.resolver.resolve("agent1qt0487g004xezsz8cte4827t7y847gzr5n6enwv6rrk09l0cjfl62rmnx79")
+    def test_resolve_normal(self):
+        expected_agent = "agent1qt0487g004xezsz8cte4827t7y847gzr5n6enwv6rrk09l0cjfl62rmnx79"
+        destination, endpoints = await self.resolver.resolve(expected_agent)
         self.assertEqual(len(endpoints), 10)
-        self.assertEqual(destination, "agent1qt0487g004xezsz8cte4827t7y847gzr5n6enwv6rrk09l0cjfl62rmnx79")
+        self.assertEqual(destination, expected_agent)
 
+    def test_resolve_not_registered(self):
         not_registered_agent = "agent1q992tl7j9062ussaunq6vp5p4qaqugkzp8pva3xlszmeg0533k952lz8alf"
         destination2, endpoints2 = await self.resolver.resolve(not_registered_agent)
         self.assertEqual(len(endpoints2), 0)
