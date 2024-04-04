dc0b240fcb5f2c94e8f7dce678e1f26d0a57bace
James Riehl <james.riehl@fetch.ai>
2024-04-04 13:10:00 +0100

    fix(#2): Updated unit tests for RulesBasedResolver.

--- python/tests/test_resolver.py
+++ python/tests/test_resolver.py
@@ -1,8 +1,9 @@
+import asyncio
 import unittest
 from uagents.resolver import RulesBasedResolver
 
 
-class TestRulesBasedResolver(unittest.TestCase):
+class TestRulesBasedResolver(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.resolver = RulesBasedResolver(
             rules={
@@ -10,13 +11,13 @@ class TestRulesBasedResolver(unittest.TestCase):
             }
         )
 
-    def test_resolve_normal(self):
+    async def test_resolve_normal(self):
         expected_agent = "agent1qt0487g004xezsz8cte4827t7y847gzr5n6enwv6rrk09l0cjfl62rmnx79"
         destination, endpoints = await self.resolver.resolve(expected_agent)
         self.assertEqual(len(endpoints), 10)
         self.assertEqual(destination, expected_agent)
 
-    def test_resolve_not_registered(self):
+    async def test_resolve_not_registered(self):
         not_registered_agent = "agent1q992tl7j9062ussaunq6vp5p4qaqugkzp8pva3xlszmeg0533k952lz8alf"
         destination2, endpoints2 = await self.resolver.resolve(not_registered_agent)
         self.assertEqual(len(endpoints2), 0)
